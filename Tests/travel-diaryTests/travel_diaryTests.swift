dc7369765f57efca2f46562f71bbec4f5df4b35e
MonsterWison <monsterbb100@gmail.com>
2025-07-15 20:43:07 +0800

    Regroup project folders

---
+++ Tests/travel-diaryTests/travel_diaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  travel_diaryTests.swift
+//  travel-diaryTests
+//
+//  Created by Wilson Ho on 24/6/2025.
+//
+
+import XCTest
+@testable import travel_diary
+
+class travel_diaryTests: XCTestCase {
+
+    func testExample() throws {
+        // Write your test here and use APIs like `XCTAssert(...)` to check expected conditions.
+    }
+
+}
